fd43958e88ab4f0f3990ea21123e1c72d5510dfc
Максим Башкатов <maksymbashkatov@gmail.com>
2022-08-01 23:24:37 +0300

    Was added task model to admin panel.

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from task.models import Task
 
-# Register your models here.
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'text', 'deadline', 'status', 'priority', 'importance', 'user']
+    search_fields = ['name', 'text', 'deadline', 'status', 'priority', 'importance']
+    list_filter = ['name', 'text', 'deadline', 'status', 'priority', 'importance', 'user']
+    list_editable = ['name', 'text', 'deadline', 'status', 'priority', 'importance', 'user']
+
+    class Meta:
+        model = Task
+
+
+admin.site.register(Task, TaskAdmin)
