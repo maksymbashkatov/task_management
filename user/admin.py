683298c67f9069d140f7316ed345ed2da9343f68
Максим Башкатов <maksymbashkatov@gmail.com>
2022-08-01 22:58:01 +0300

    Was created admin panel. Was created CustomUserManager, modified CustomUser model (deleted username field, added custom manager).

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from user.models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'first_name', 'last_name', 'work_position', 'password', 'is_active']
+    search_fields = ['email', 'first_name', 'last_name', 'work_position', 'is_active']
+    list_filter = ['first_name', 'last_name', 'work_position', 'is_active']
+    list_editable = ['first_name', 'last_name', 'work_position', 'password', 'is_active']
+
+    class Meta:
+        model = CustomUser
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- user/models.py
+++ user/models.py
@@ -1,13 +1,52 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    work_position = models.CharField(max_length=50)
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
 
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email, and password.
+        """
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    work_position = models.CharField(max_length=50, blank='true', null='true')
+
+    objects = CustomUserManager()
+
+    USERNAME_REQUIRED = False
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
         db_table = 'users'
+        verbose_name = 'user'
