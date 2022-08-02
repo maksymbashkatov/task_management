9e6a4da411b57aaa588b02270c23526ce868b4ab
Максим Башкатов <maksymbashkatov@gmail.com>
2022-08-02 15:58:50 +0300

    Was added task list controller, urls and template. Was added link from task detail to tasks list.

--- task/urls.py
+++ task/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from task.views import TaskDetailView
+from task.views import TaskDetailView, TaskListView
 
 urlpatterns = [
-    path('task/<int:pk>', TaskDetailView.as_view(), name='task_detail')
+    path('task/<int:pk>', TaskDetailView.as_view(), name='task_detail'),
+    path('all/', TaskListView.as_view(), name='task_list')
 ]
--- task/views.py
+++ task/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from task.models import Task
 
 
 class TaskDetailView(DetailView):
     model = Task
     template_name = 'task/task_detail.html'
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'task/task_list.html'
