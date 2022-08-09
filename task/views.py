4db787d250fb4bcf668da942ea67a92e74dc5e39
Максим Башкатов <maksymbashkatov@gmail.com>
2022-08-09 10:42:14 +0300

    Was added function for check user.

--- task/views.py
+++ task/views.py
@@ -3,23 +3,23 @@ from django.views.generic import DetailView, ListView
 from task.models import Task
 
 
+def render_check_user(request, template_for_checked_user):
+    u = request.user
+    if u.is_authenticated:
+        return render(request, template_for_checked_user if u.is_confirmed else 'user/confirm_user.html')
+    else:
+        return render(request, 'user/not_authorized.html')
+
+
 class TaskDetailView(DetailView):
     model = Task
 
     def get(self, request, *args, **kwargs):
-        u = request.user
-        if u.is_authenticated:
-            return render(request, 'task/task_detail.html' if u.is_confirmed else 'user/confirm_user.html')
-        else:
-            return render(request, 'user/not_authorized.html')
+        return render_check_user(request, 'task/task_detail.html')
 
 
 class TaskListView(ListView):
     model = Task
 
     def get(self, request, *args, **kwargs):
-        u = request.user
-        if u.is_authenticated:
-            return render(request, 'task/task_list.html' if u.is_confirmed else 'user/confirm_user.html')
-        else:
-            return render(request, 'user/not_authorized.html')
+        return render_check_user(request, 'task/task_list.html')
