8f991fa03a427c53f9db0017fcc957fdb1a3ef20
Максим Башкатов <maksymbashkatov@gmail.com>
2022-08-10 21:52:34 +0300

    Was finished work with users and user in admin.

--- user/admin.py
+++ user/admin.py
@@ -1,21 +1,45 @@
 from django.contrib import admin
-from user.forms import CustomUserCreationForm
+from task.models import Task
 from user.models import CustomUser, UserUUID
 
 
+class TaskInline(admin.TabularInline):
+    model = Task
+
+    def short_text(self, obj):
+        return f'{obj.text[:10]}...'
+
+    fields = ('name', 'short_text')
+    readonly_fields = ('short_text',)
+
+
 class CustomUserAdmin(admin.ModelAdmin):
-    add_form_template = CustomUserCreationForm
-    list_display = ['id', 'email', 'first_name', 'last_name', 'work_position', 'password', 'is_active', 'is_confirmed']
-    search_fields = ['email', 'first_name', 'last_name', 'work_position', 'is_active']
-    list_filter = ['first_name', 'last_name', 'work_position', 'is_active']
-    list_editable = ['first_name', 'last_name', 'work_position', 'password', 'is_active', 'is_confirmed']
+
+    inlines = (TaskInline,)
+
+    def total_tasks(self, obj):
+        return Task.objects.filter(user=obj).count()
+
+    # List of CustomUser fields
+    # ('id', 'email', 'first_name', 'last_name', 'work_position', 'password', 'is_active', 'is_confirmed')
+
+    # List of users form
+    list_display = ('first_name', 'last_name', 'work_position', 'email', 'total_tasks')
+    search_fields = ('first_name', 'last_name')
+
+    # User form
+    fields = ('first_name', 'last_name', 'email', 'total_tasks')
+    readonly_fields = ('total_tasks',)
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
     class Meta:
         model = CustomUser
 
 
 class UserUUIDAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user']
+    list_display = ('id', 'user')
 
     class Meta:
         model = UserUUID
